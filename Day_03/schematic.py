2e327e9d2460ad38b2908f13975e79fe1afa2f39
Nyika Sándor <35967707+nyikasandor@users.noreply.github.com>
2023-12-04 23:45:43 +0000

    Ugly solution for Day3

--- Day_03/schematic.py
+++ Day_03/schematic.py
@@ -4,7 +4,10 @@ import re
 class Line:
 
     def __init__(self, line) -> None:
+        self.text = line
         self.numbers = Line.find_numbers(line)
+        self.special_chars = Line.find_special_chars(line)
+        self.gears = Line.find_gears(line)
 
     @classmethod
     def find_special_chars(cls, line):
@@ -17,7 +20,57 @@ class Line:
         return [(range(*match.span()), match.group())
                 for match in re.finditer(pattern, line)]
 
-    def get_number_in_pos(self, pos):
+    @classmethod
+    def add_up(cls, lines, extraction_method):
+        sum = 0
+        adj = [Line(''), Line('')]
+        for line in lines:
+            sum += extraction_method(adj[1], [adj[0], line])
+            adj.append(line)
+            adj.pop(0)
+        return sum
+
+    @classmethod
+    def add_up_schematic(cls, lines):
+        return cls.add_up(lines, Line.value_of_line)
+
+    @classmethod
+    def add_up_gears(cls, lines):
+        return cls.add_up(lines, Line.gear_ratios)
+
+    @classmethod
+    def find_gears(cls, line):
+        return [match.span()[0] for match in re.finditer(r'\*', line)]
+
+    def gear_ratios(self, adjacent_lines):
+        sum = 0
+        for gear in self.gears:
+            numbers = []
+            for line in [adjacent_lines[0], self, adjacent_lines[1]]:
+                for idx in [gear - 1, gear, gear + 1]:
+                    number = line.get_numbers_in_position(idx)
+                    if number != 0:
+                        numbers.append(number)
+            if len(numbers) == 2:
+                sum += numbers[0] * numbers[1]
+        return sum
+
+    def value_of_line(self, adjacent_lines):
+        value = 0
+        for special_char in self.special_chars:
+            for line in [*adjacent_lines, self]:
+                value += sum([line.get_numbers_in_position(pos) for pos
+                              in [special_char - 1, special_char,
+                                  special_char + 1]])
+        return value
+
+    def fetch_numbers_in_position(self, pos):
+        for span, number in self.numbers:
+            if pos in span:
+                return int(number)
+        return 0
+
+    def get_numbers_in_position(self, pos):
         for span, number in self.numbers:
             if pos in span:
                 self.numbers.remove((span, number))
@@ -25,4 +78,4 @@ class Line:
         return 0
 
     def get_numbers_in_positions(self, positions):
-        return sum(self.get_number_in_pos(pos) for pos in positions)
+        return 
--- Day_03/schematic_test.py
+++ Day_03/schematic_test.py
@@ -12,6 +12,9 @@ class Test_game(unittest.TestCase):
     def get_test_line_2(self):
         return '...*....!.'
 
+    def get_test_line_3(self):
+        return '..10....!.'
+
     def test_can_find_numbers(self):
         line = self.get_test_line_1()
         numbers = Line.find_numbers(line)
@@ -20,19 +23,44 @@ class Test_game(unittest.TestCase):
 
     def test_can_lookup_no_number(self):
         line = Line(self.get_test_line_1())
-        self.assertEqual(line.get_number_in_pos(3), 0)
+        self.assertEqual(line.get_numbers_in_position(3), 0)
 
     def test_can_lookup_number(self):
         line = Line(self.get_test_line_1())
-        self.assertEqual(line.get_number_in_pos(1), 467)
+        self.assertEqual(line.get_numbers_in_position(1), 467)
 
     def test_can_find_special_chars_in_line(self):
         line = self.get_test_line_2()
         self.assertEqual(Line.find_special_chars(line), [3, 8])
 
-    def test_can_lookup_multiple_numbers(self):
-        line = Line(self.get_test_line_1())
-        self.assertEqual(line.get_numbers_in_positions([1, 6, 5]), 467 + 114)
+    def test_can_find_value_of_lines(self):
+        adjacent_lines = [Line(self.get_test_line_1())]
+        line = Line(self.get_test_line_2())
+        self.assertEqual(line.value_of_line(adjacent_lines), 467 + 114)
+
+    def test_can_solve_puzzle_1(self):
+        input_path = os.path.join(os.path.dirname(__file__), 'input_1.txt')
+        with open(input_path) as f:
+            document = f.read()
+        lines = [Line(line) for line in document.split('\n')]
+        self.assertEqual(Line.add_up_schematic(lines), 559667)
+
+    def test_can_find_gears(self):
+        line = self.get_test_line_2()
+        self.assertEqual(Line.find_gears(line), [3])
+
+    def test_can_calculate_gear_ratio(self):
+        adjacent_lines = [Line(self.get_test_line_1()),
+                          Line(self.get_test_line_3())]
+        current = Line(self.get_test_line_2())
+        self.assertEqual(current.gear_ratios(adjacent_lines), 467 * 10)
+
+    def test_can_solve_puzzle_2(self):
+        input_path = os.path.join(os.path.dirname(__file__), 'input_1.txt')
+        with open(input_path) as f:
+            document = f.read()
+        lines = [Line(line) for line in document.split('\n')]
+        self.assertEqual(Line.add_up_gears(lines), 86841457)
 
 
 if __name__ == '__main__':
