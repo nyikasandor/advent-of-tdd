ed6564c3cf3668ac878bf63c58a6aaffd8c90881
Nyika Sándor <35967707+nyikasandor@users.noreply.github.com>
2023-12-07 23:15:27 +0000

    Stuck wit Day07 pt2

---
+++ Day_07/hand.py
@@ -0,0 +1,101 @@
+class Hand:
+
+    __power = {
+        'high': 0,
+        'one pair': 1,
+        'two pair': 2,
+        'three of a kind': 3,
+        'full house': 4,
+        'four of a kind': 5,
+        'five of a kind': 6,
+    }
+
+    __cards_joker = list(reversed(
+            ['A', 'K', 'Q', 'T',
+             '9', '8', '7', '6', '5',
+             '4', '3', '2', 'J']))
+
+    __cards = list(reversed(
+                ['A', 'K', 'Q', 'J', 'T',
+                 '9', '8', '7', '6', '5',
+                 '4', '3', '2']))
+
+    __card_value = dict(zip(__cards,
+                            range(len(__cards))))
+
+    __card_joker_value = dict(zip(__cards_joker,
+                                  range(len(__cards_joker))))
+
+    def __init__(self, input_line) -> None:
+        self.cards, bid_ = input_line.split()
+        self.bid = int(bid_)
+        self.type = self.calculate_type()
+        self.power = Hand.__power[self.type]
+        self.decoded_cards = self.decode_cards()
+        self.joker_power = self.calculate_joker_power()
+        self.decoded_joker_cards = self.decode_joker_cards()
+
+    def __repr__(self) -> str:
+        return f'{self.cards}, {self.joker_power}'
+
+    def calculate_joker_power(self):
+        joker_count = self.cards.count('J')
+        joker_gain = joker_count
+        if self.power < 4 and joker_count + self.power > 3:
+            # skip full house score
+            joker_gain += 1
+        if joker_count > 2 and self.power != 4:
+            return 5
+        return min(self.power + joker_gain, 6)
+
+    def calculate_type(self):
+        unique_cards = len(set(self.cards))
+        if 5 == unique_cards:
+            return 'high'
+        if 4 == unique_cards:
+            return 'one pair'
+        if (3 == unique_cards and
+            (self.cards.count(self.cards[0]) == 3 or
+             self.cards.count(self.cards[1]) == 3 or
+             self.cards.count(self.cards[2]) == 3)):
+            return 'three of a kind'
+        if 3 == unique_cards:
+            return 'two pair'
+        if (2 == unique_cards and (
+             self.cards.count(self.cards[1]) == 4 or
+             self.cards.count(self.cards[2]) == 4)):
+            return 'four of a kind'
+        if 2 == unique_cards:
+            return 'full house'
+        if 1 == unique_cards:
+            return 'five of a kind'
+
+    def decode_cards(self):
+        return [Hand.__card_value[card] for card in self.cards]
+
+    def decode_joker_cards(self):
+        return [Hand.__card_joker_value[card] for card in self.cards]
+
+    @classmethod
+    def sort_hands(cls, hands):
+        hands.sort(key=lambda hand: (hand.power,
+                                     *hand.decoded_cards))
+
+    @classmethod
+    def sort_joker_hands(cls, hands):
+        hands.sort(key=lambda hand: (hand.joker_power,
+                                     *hand.decoded_joker_cards))
+
+    @classmethod
+    def calculate_winnings(cls, hands):
+        Hand.sort_hands(hands)
+        # [print(hand) for rank, hand in enumerate(hands, 1)]
+        return sum(rank * hand.bid
+                   for rank, hand in enumerate(hands, 1))
+
+    @classmethod
+    def calculate_joker_winnings(cls, hands):
+        Hand.sort_joker_hands(hands)
+        [print(hand) for rank, hand in enumerate(hands, 1)]
+        return sum(rank * hand.bid
+                   for rank, hand in enumerate(hands, 1))
---
+++ Day_07/hand_test.py
@@ -0,0 +1,118 @@
+import unittest
+import os
+
+from hand import Hand
+
+
+class Test_hand(unittest.TestCase):
+
+    def get_hand_pair(self):
+        return '42T4K 765'
+
+    def get_hand_two_pair(self):
+        return '3KT3K 765'
+
+    def get_hand_three_of_a_kind(self):
+        return '3233K 765'
+
+    def get_hand_full_house(self):
+        return '32233 765'
+
+    def get_hand_four_of_a_kind(self):
+        return '55455 765'
+
+    def get_hand_five_of_a_kind(self):
+        return '55555 765'
+
+    def get_hand_hight_cards(self):
+        return '23456 765'
+
+    def test_can_create_hand(self):
+        hand = Hand(self.get_hand_pair())
+        self.assertEqual(hand.bid, 765)
+        self.assertEqual(hand.cards, '42T4K')
+
+    def test_can_detect_one_pair(self):
+        hand = Hand(self.get_hand_pair())
+        self.assertEqual(hand.type, 'one pair')
+
+    def test_can_detect_two_pair(self):
+        hand = Hand(self.get_hand_two_pair())
+        self.assertEqual(hand.type, 'two pair')
+
+    def test_can_detect_three_of_a_kind(self):
+        hand = Hand(self.get_hand_three_of_a_kind())
+        self.assertEqual(hand.type, 'three of a kind')
+
+    def test_can_detect_full_house(self):
+        hand = Hand(self.get_hand_full_house())
+        self.assertEqual(hand.type, 'full house')
+
+    def test_can_detect_four_of_a_kind(self):
+        hand = Hand(self.get_hand_four_of_a_kind())
+        self.assertEqual(hand.type, 'four of a kind')
+
+    def test_can_detect_five_of_a_kind(self):
+        hand = Hand(self.get_hand_five_of_a_kind())
+        self.assertEqual(hand.type, 'five of a kind')
+
+    def test_can_detect_high(self):
+        hand = Hand(self.get_hand_hight_cards())
+        self.assertEqual(hand.type, 'high')
+
+    def test_can_detect_four_joker(self):
+        hand = Hand('JJJ6Q 1')
+        self.assertEqual(hand.joker_power, 5)
+
+    def test_can_detect_pair_joker(self):
+        hand = Hand('J236Q 1')
+        self.assertEqual(hand.joker_power, 1)
+
+    def test_can_detect_drill_joker(self):
+        hand = Hand('J2J6Q 1')
+        self.assertEqual(hand.joker_power, 3)
+
+    def test_can_detect_four_joker_pair(self):
+        hand = Hand('J2J66 1')
+        self.assertEqual(hand.joker_power, 5)
+
+    def test_can_detect_five_joker(self):
+        hand = Hand('J2J2J 1')
+        self.assertEqual(hand.joker_power, 6)
+
+    def test_can_label_strength(self):
+        hands = [Hand('KK677 1'),
+                 Hand('KTJ2T 2'),]
+        Hand.sort_hands(hands)
+        self.assertEqual(hands[0].cards, 'KTJ2T')
+        self.assertEqual(hands[1].cards, 'KK677')
+
+    def test_can_sort(self):
+        hands = [Hand(self.get_hand_hight_cards()),
+                 Hand(self.get_hand_five_of_a_kind()),
+                 Hand(self.get_hand_three_of_a_kind()),
+                 Hand(self.get_hand_four_of_a_kind()),
+                 Hand(self.get_hand_full_house()),
+                 Hand(self.get_hand_two_pair()),
+                 Hand(self.get_hand_pair()),]
+        Hand.sort_hands(hands)
+        self.assertEqual(hands[0].type, 'high')
+        self.assertEqual(hands[6].type, 'five of a kind')
+
+    def test_can_solve_puzzle_1(self):
+        input_path = os.path.join(os.path.dirname(__file__), 'input_1.txt')
+        with open(input_path) as f:
+            lines = f.read().splitlines()
+        hands = [Hand(line) for line in lines]
+        self.assertEqual(Hand.calculate_winnings(hands), 247823654)
+
+    def test_can_solve_puzzle_2(self):
+        input_path = os.path.join(os.path.dirname(__file__), 'input_1.txt')
+        with open(input_path) as f:
+            lines = f.read().splitlines()
+        hands = [Hand(line) for line in lines]
+        self.assertEqual(Hand.calculate_joker_winnings(hands), -1)
+
+
+if __name__ == '__main__':
+    unittest.main()
